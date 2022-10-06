8b069c645433b23c0eaae4dc68396105d217e654
Henry Crossley <crossh2001@yahoo.com>
2022-10-05 22:36:05 -0700

    hyperlink nav works

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const url = require('url');
+const http = require('http');
+
+ 
+http.createServer(function(request, response){
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.writeFile("<html>");
+    response.writeFile("<ul>");
+    response.writeFile("<li>");
+    response.writeFile("dlkajdfl;dfsla");
+    response.writeFile("</li>");
+    response.writeFile("</ul>");
+    response.writeFile("</html>");  
+    response.end(); 
+})
 
 app.get('/', (req,res) =>{
 
@@ -11,6 +25,14 @@ app.get('/', (req,res) =>{
         res.send(data);
     });
 });
+
+app.get('/about',(req,res)=>{
+    fs.readFile("./about.html","utf8", function(err, data){
+        if(err) throw err;
+
+        res.send(data);
+    })
+})
 
 app.listen(3333, ()=>{
     console.log("I am listing to you");
